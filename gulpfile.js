c1004a4eb4e8211a06c934bfa6c44ad20d56fdca
Imad Collin <Admin@student214-167.eduroam.uu.se>
2018-04-25 14:11:41 +0200

    Remove browser from gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ gulp.task('browser-sync', ['nodemon'], function () {
     browserSync.init(null, {
         proxy: "http://localhost:3000",
         files: ["public/**/*.*"],
-        browser:"google chrome",
         port: 5000,
     });
 });
