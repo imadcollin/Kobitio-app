ee4d4ea37b44a2f613f4a149a99a93bcac9d6243
Imad Collin <Admin@student245-131.eduroam.uu.se>
2018-04-26 11:01:40 +0200

    Integrate local db setting

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
+
 //***************************** MLab configuration *****************************/
 var APIKey="9Kc-lKEdig09j-lzqfuaXwDLjKX5a6qO";//key
 const MONGO_URL ="mongodb://mlap:mlap123456@ds255309.mlab.com:55309/uip";//url
 var db_collection="https://api.mlab.com/api/1/databases/uip/collections/users"//collection
 var db_document=`${db_collection}?apiKey=${APIKey}`;//document 
+
 
 //***************************** Get data  *****************************/
 function httpGet(theUrl)
@@ -28,6 +30,7 @@ var mock_user=[{
 }];
 //For delete a user... 
 var user_id='5ae0f771bd966f59538644bd';
+
 
 //***************************** Post data  *****************************/
 function httpPost(theUrl)
@@ -68,21 +71,6 @@ xhr.onload = function () {
 xhr.send(JSON.stringify(mock_user));
 
 }
-var models=require('./mongo_model');
-var connector=require('./mongo_onnector');
-
-
-//export
-//Set up default mongoose connection
-
-connector.db().on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-models.teacher_model().find({}, function(err, users) {
-	if (err) throw err;
-  
-	// object of all the users
-	console.log(users);
-  });
 
 //let user_data= httpGet(db_document);
 //console.log(JSON.parse(user_data));//dummy data fetch....
@@ -94,6 +82,36 @@ models.teacher_model().find({}, function(err, users) {
 //httpDelete(db_document);
 
 
+//***************************** Local database connection  *****************************/
+
+/**
+ * What this is for?
+ * This is the default setting to the local database using mongoose 
+ * there are two another files one is the connector and another is model 
+ * This two files are require to be able to connect the local mongo db 
+ * The example below is showing how to fetch the data from the local db on your machine 
+ * Note: 
+ * Dont forget to add one field of dummy dta to your local db . 
+ */
+
+var models=require('./mongo_model');
+var connector=require('./mongo_onnector');
+
+function fetch_local_data(){
+//Set up default mongoose connection
+connector.db().on('error', console.error.bind(console, 'MongoDB connection error:'));
+//Bring the module from mongo_model.
+models.teacher_model().find({}, function(err, users) {
+	if (err) throw err;
+  
+	// object of all the users
+	console.log(users);
+  });
+}
+
+//fetch_local_data();
+
+
 //***************************** Express serve files  *****************************/
 app.use(express.static('public'));
 app.get('/', function(req, res) {
@@ -101,3 +119,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(3000);/*PORT 3000 is opened*/
+
+
