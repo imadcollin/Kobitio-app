71edf34198fb51fb42d6cfa7693db7ceff18cc60
kunalgulati <kunalgulati98@gmail.com>
2018-04-26 12:34:22 +0200

    Profile layout Changed

--- public/js/profile.js
+++ public/js/profile.js
@@ -28,3 +28,63 @@ $(".close").click(function() {
 function translate (index) {
     $("#page_title").text(page_title[index]);
 }
+
+
+// Code for Edit profile pop-up window and form
+// Get the modal
+var modal = document.getElementById('editProfileModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("editProfile");
+
+
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+
+// Edit Settings Pop-up Window 
+
+var SettingModal = document.getElementById('editSettingeModal');
+
+// Get the button that opens the modal
+var EditSettingButton = document.getElementById("editSetting");
+
+// Get the <span> element that closes the modal
+
+// When the user clicks the button, open the modal 
+EditSettingButton.onclick = function() {
+    SettingModal.style.display = "block";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == SettingModal) {
+        SettingModal.style.display = "none";
+    }
+}
+
+
+
+// Get the <span> element that closes the modal
+var close = document.getElementById("close-form");
+// When the user clicks on <span> (x), close the modal
+close.onclick = function() {
+
+	//getting the id of main modal
+	parentDiv = document.getElementById("close-form").parentElement.parentElement.parentElement.parentElement;
+    parentDiv.style.display = "none";
+
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
