3b5a280a1cf30e6b4b754d1813f6177eec1aae84
Imad Collin <Admin@MacBook.local>
2018-05-06 00:52:27 +0200

    Add service worker

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// App.js
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import CreateComponent from './components/CreateComponent';
+import EditComponent from './components/EditComponent';
+import IndexComponent from './components/IndexComponent';
+
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Express Tutorial</h2>
+          <ul>
+          <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/create'}>Create</Link></li>
+            <li><Link to={'/index'}>List</Link></li>
+            <li><Link to={'/edit/:id'}>Edit</Link></li>
+          </ul>
+          <hr />
+          <Switch>
+              <Route exact path='/create' component={CreateComponent} />
+              <Route path='/edit/:id' component={EditComponent} />
+              <Route path='/index' component={IndexComponent} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
