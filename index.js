32900c435f45e3dd22a12679b2ff0f9ad81e1a77
Imad Collin <Admin@MacBook.local>
2018-05-05 02:17:12 +0200

    Replace the mock db with the right

--- index.js
+++ index.js
@@ -1,105 +1,109 @@
 'use strict';
-var express = require('express');
-var app = express();
-var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
+let express = require('express');
+let app = express();
+let router = express.Router();
+let MongoClient = require('mongodb').MongoClient;
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 //***************************** MLab configuration *****************************/
-var APIKey="9Kc-lKEdig09j-lzqfuaXwDLjKX5a6qO";//key
-const MONGO_URL ="mongodb://mlap:mlap123456@ds255309.mlab.com:55309/uip";//url
-var user_collection="https://api.mlab.com/api/1/databases/uip/collections/users"//collection
-var collection_url=`${user_collection}?apiKey=${APIKey}`;//document 
+const APIKey = "9Kc-lKEdig09j-lzqfuaXwDLjKX5a6qO"; //key
+const MONGO_URL = "mongodb://mlap:mlap123456@ds255309.mlab.com:55309/uip"; //url
+const collections = "https://api.mlab.com/api/1/databases/uip/collections/";
 
+let config = {
+	user: `${collections}/user`,
+	userInfo: `${collections}/userInfo`,
+	deedsHistory: `${collections}/deedsHistory`,
+	relations: `${collections}/relations`,
+	deeds: `${collections}/deeds`
 
-//***************************** Get data  *****************************/
-function httpGet(theUrl)
-{
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-        xmlHttp.send( null );
-    return xmlHttp.responseText;
 }
+//***************************** Get data  *****************************/
+function httpGet(theUrl) {
+	let xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", `${theUrl}?apiKey=${APIKey}`, false); // false for synchronous request
+	xmlHttp.send(null);
+	return xmlHttp.responseText;
+}
+
+/*** Usage example ****/
+/*Get all users from the given collection_url*/
+let User = httpGet(config.user);
+console.log(JSON.parse(User)); //dummy data fetch....
+
 
 //***************************** Get User details based on name  *****************************/
 //link to full documentation: http://docs.mlab.com/data-api/
-function getUser(user_name){
-	var url=`https://api.mlab.com/api/1/databases/uip/collections/users?q={"name":"${user_name}"}&apiKey=9Kc-lKEdig09j-lzqfuaXwDLjKX5a6qO`
-	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", url, false ); // false for synchronous request
-        xmlHttp.send( null );
-    return xmlHttp.responseText;
+function getUser(user_name) {
+	var url = `${config.user}?q={"username":"${user_name}"}&apiKey=${APIKey}`
 
-
+	let xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", url, false); // false for synchronous request
+	xmlHttp.send(null);
+	return xmlHttp.responseText;
 }
+/*** Usage example ****/
+//let UserName= getUser("adam1990"); 
+//console.log(UserName);//dummy data fetch....
+
 
 //***************************** Mock User data  *****************************/
-var mock_user=[{
-    "id": 4,
-    "name": "posted",
-    "age": 28,
-    "gender": "ss"
+let mock_user = [{
+	"username": "bob12",
+	"password": "password",
+	"email": "bob@email.com"
 }];
 //For delete a user... 
-var user_id='5aecadfaf707015922a75a59';
+let user_id = '5aecf643bd966f1715367d8a';
 
 
 //***************************** Post data  *****************************/
 //link to full documentation: http://docs.mlab.com/data-api/
 
-function httpPost(theUrl,user)
-{
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
+function httpPost(theUrl, user) {
+	let xhr = new XMLHttpRequest();
 
-xhr.open("POST", theUrl, true);
-xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
-xhr.onload = function () {
-	var users = JSON.parse(xhr.responseText);
-	if (xhr.readyState == 4 && xhr.status == "201") {
-		console.log(users);
-	} else {
-		console.error(users);
+	xhr.open("POST", `${theUrl}?apiKey=${APIKey}`, true);
+	xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
+	xhr.onload = function () {
+		let users = JSON.parse(xhr.responseText);
+		if (xhr.readyState == 4 && xhr.status == "201") {
+			console.log(users);
+		} else {
+			console.error(users);
+		}
 	}
-}
-xhr.send(JSON.stringify(user));
+	xhr.send(JSON.stringify(user));
 
 }
 
 //***************************** Delete data  *****************************/
 //link to full documentation: http://docs.mlab.com/data-api/
 
-function httpDelete(theUrl,user_id)
-{
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
+function httpDelete(theUrl, user_id) {
+	let xhr = new XMLHttpRequest();
 
-xhr.open("DELETE", `${theUrl}/${user_id}?apiKey=${APIKey}`, true);
-xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
-xhr.onload = function () {
-	var users = JSON.parse(xhr.responseText);
-	if (xhr.readyState == 4 && xhr.status == "201") {
-		console.log(users);
-	} else {
-		console.error(users);
+	xhr.open("DELETE", `${theUrl}/${user_id}?apiKey=${APIKey}`, true);
+	xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
+	xhr.onload = function () {
+		let users = JSON.parse(xhr.responseText);
+		if (xhr.readyState == 4 && xhr.status == "201") {
+			console.log(users);
+		} else {
+			console.error(users);
+		}
 	}
-}
-xhr.send(JSON.stringify(mock_user));
+	xhr.send(JSON.stringify(mock_user));
 
 }
-/*Get all users from the given collection_url*/
-let user_data= httpGet(collection_url); 
-console.log(JSON.parse(user_data));//dummy data fetch....
-/*Query user using name....   */
-//let user_data= getUser("bob"); 
-//console.log(user_data);//dummy data fetch....
+
+
 
 //Call post method! 
-//httpPost(collection_url,mock_user);
+//httpPost(config.user,mock_user);
 
 //Call delete method! 
-httpDelete(user_collection,user_id);
+//httpDelete(config.user,user_id);
 
 
 //***************************** Local database connection  *****************************/
@@ -114,19 +118,19 @@ httpDelete(user_collection,user_id);
  * Dont forget to add one field of dummy dta to your local db . 
  */
 
-var models=require('./mongo_model');
-var connector=require('./mongo_onnector');
+let models = require('./mongo_model');
+let connector = require('./mongo_onnector');
 
-function fetch_local_data(){
-//Set up default mongoose connection
-connector.db().on('error', console.error.bind(console, 'MongoDB connection error:'));
-//Bring the module from mongo_model.
-models.teacher_model().find({}, function(err, users) {
-	if (err) throw err;
-  
-	// object of all the users
-	console.log(users);
-  });
+function fetch_local_data() {
+	//Set up default mongoose connection
+	connector.db().on('error', console.error.bind(console, 'MongoDB connection error:'));
+	//Bring the module from mongo_model.
+	models.teacher_model().find({}, function (err, users) {
+		if (err) throw err;
+
+		// object of all the users
+		console.log(users);
+	});
 }
 
 //fetch_local_data();
@@ -134,10 +138,8 @@ models.teacher_model().find({}, function(err, users) {
 
 //***************************** Express serve files  *****************************/
 app.use(express.static('public'));
-app.get('/', function(req, res) {
-    res.sendfile('./public/index.html');
+app.get('/', function (req, res) {
+	res.sendfile('./public/index.html');
 });
 
-app.listen(3000);/*PORT 3000 is opened*/
-
-
+app.listen(3000); /*PORT 3000 is opened*/
