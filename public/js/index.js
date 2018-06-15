39e4724f0f8290052ce91efeaaf721194453eb8a
imadcollin <imad.collin@gmail.com>
2018-06-15 14:55:35 +0200

    Redirect on login

--- public/js/index.js
+++ public/js/index.js
@@ -4,37 +4,37 @@
  */
 
 /*INDEX PAGE SCRIPTS
-* All scripts related to the index page. Each page has their own scripts in a single js document.
-* The methods translate() is unique for each individual page.
-*/
+ * All scripts related to the index page. Each page has their own scripts in a single js document.
+ * The methods translate() is unique for each individual page.
+ */
 /*Language Translation index*/
-if (localStorage.getItem("index") == null){
-    localStorage.setItem("index",0)
+if (localStorage.getItem("index") == null) {
+    localStorage.setItem("index", 0)
 }
 
 /*Pop up windows*/
-$("#login_button").click(function() {
+$("#login_button").click(function () {
     $("#overlay").removeClass("hidden");
     $("#loginWindow").removeClass("hidden");
 });
 
-$("#signup_button").click(function() {
+$("#signup_button").click(function () {
     $("#overlay").removeClass("hidden");
     $("#signupWindow").removeClass("hidden");
 });
 
-$("#give_points").click(function() {
+$("#give_points").click(function () {
     $("#overlay").removeClass("hidden");
     $("#giveWindow").removeClass("hidden");
 });
 
-$(".close").click(function() {
+$(".close").click(function () {
     $("#overlay").addClass("hidden");
     $("#loginWindow").addClass("hidden");
     $("#signupWindow").addClass("hidden");
 });
 
-function translate (index) {
+function translate(index) {
     $("#page_title").text(page_title[index]);
     $("#mission").text(mission[index]);
     $("#login_button").text(login_button[index]);
@@ -42,28 +42,18 @@ function translate (index) {
 
 }
 
+function login() {
+    let user_name = document.getElementById("userName").value;
+    let password = document.getElementById("Password").value;
 
+   if( user_name == "adam_1990" && password == "123456") {
 
+       window.location.href = "profile.html"
+   }else{
+       alert('Loaded but Something went wrong Please provide valid Password or UserName');
+        window.location.href = "index.html"
+   }
+}
+// var delay = 2000;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+setTimeout(test, delay)
